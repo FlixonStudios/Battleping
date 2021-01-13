0665107ed348d60fce9f594665de1e03494836bb
FlixonStudios <chiakaiming@gmail.com>
2021-01-13 20:00:45 +0800

    Many many changes

---
+++ Assets/Scripts/MoveByTouchBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveByTouchBehaviour : MonoBehaviour
+{
+
+    [SerializeField] float baseMoveSpeed = 10.0f;
+    [SerializeField] float baseMaxHorizontalMoveSpeed = 5f;
+    [SerializeField] float baseMaxVerticalMoveSpeed = 5f;
+    [SerializeField] Joystick joystick;
+
+    private float currentMoveSpeed;
+    private Rigidbody2D myBody;
+
+    private void Start()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        currentMoveSpeed = baseMoveSpeed;
+
+    }
+
+    private void Update()
+    {
+        MovePlayerHorizontally();
+        MovePlayerVertically();
+        ThrottlePlayerVelocity();
+    }
+
+    public void MovePlayerHorizontally()
+    {
+        float playerXInput = joystick.Horizontal * currentMoveSpeed;
+        myBody.AddForce(new Vector2(playerXInput,0), ForceMode2D.Force);
+    }
+    public void MovePlayerVertically()
+    {
+        float playerYInput = joystick.Vertical * currentMoveSpeed;
+        myBody.AddForce(new Vector2(0, playerYInput), ForceMode2D.Force);
+    }
+    public void ThrottlePlayerVelocity()
+    {
+        float xVelocityMax = Mathf.Clamp(myBody.velocity.x, -baseMaxHorizontalMoveSpeed, baseMaxHorizontalMoveSpeed);
+        float yVelocityMax = Mathf.Clamp(myBody.velocity.x, -baseMaxVerticalMoveSpeed, baseMaxVerticalMoveSpeed);
+
+        myBody.velocity = new Vector2(xVelocityMax, yVelocityMax);        
+    }
+
+}
---
+++ Assets/Test/PlayMode/MoveByTouch_PlayTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class MoveByTouch_PlayTest
+    {       
+
+        [UnityTest]
+        public IEnumerator MoveByTouch_MoveHorizontal_Test()
+        {
+
+            float moveSpeed = 2.0f;
+            float playerInput = 1.0f;
+
+            yield return null;
+
+
+        }
+    }
+}
